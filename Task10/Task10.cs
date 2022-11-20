b1fdfe0a6287fbeb9746718024d1abe1e1990348
DariaMuravitskaya <muraviczkaya96@gmail.com>
2022-11-20 21:39:02 +0300

    solved task10

---
+++ Task10/Task10.cs
@@ -0,0 +1,11 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+ 456 -> 5
+ 782 -> 8
+ 918 -> 1 */
+
+
+
+Console.Write("Введите трехзначное число: ");
+int userInput = Convert.ToInt32(Console.ReadLine());
+string num2 = Convert.ToString(userInput);
+Console.WriteLine($"{userInput}->" +num2[1]); 
